16ec1bdf44a84896ff9fe4377a4f7b3dc59a67b4
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-10-15 15:55:22 -0300

    Updates

---
+++ precache-manifest.15cfe345e053a7b4ff445d7248149825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863a883958ce3a51eb3c892683c86179",
+    "url": "/cayama-spa/index.html"
+  },
+  {
+    "revision": "8e54a32a5d65a1a971f2",
+    "url": "/cayama-spa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5240aacc8c3999315078",
+    "url": "/cayama-spa/static/js/2.d60a407c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cayama-spa/static/js/2.d60a407c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e54a32a5d65a1a971f2",
+    "url": "/cayama-spa/static/js/main.23b10713.chunk.js"
+  },
+  {
+    "revision": "e5c3a272419e38155406",
+    "url": "/cayama-spa/static/js/runtime-main.0c0120ba.js"
+  }
+]);
